c2a201de41564fa974f97d654b058bae795c3c77
Artur Malhayan <reaze@mail.ru>
2021-01-26 18:52:49 +0300

    Add

---
+++ src/Home1.java
@@ -0,0 +1,41 @@
+public class Home1 {
+    public static void main(String[] args) {
+    float a = 4;
+    float b = 3;
+    float c = 5;
+    float d = 2;
+    float  g = a * (b+ (c / d ));
+    System.out.println(g);
+    }
+}
+/*Написать метод, вычисляющий выражение a * (b + (c / d)).
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20);{
+            System.out.println(true);
+        } else if (sum < 10 && sum > 20);{
+            System.out.println(false);
+        }
+        }
+    }
+*/
+
+  /*Метод должен вывести в консоль сообщение «Привет, указанное_имя!
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет,Артур!");
+ }
+ }
+*/
+/*
+public class Main {
+ /*
+    public static void main(String[] args){
+    }
+}
+*/
